cbbf97b9fb180055dfcc6f47c42f7951baf53fed
zhongjialiang <2511841570@qq.com>
2019-02-23 15:44:43 +0800

    增加了招聘详情

--- Readhub/src/pages/news.vue
+++ Readhub/src/pages/news.vue
@@ -29,14 +29,15 @@
       <sponsors></sponsors>
     </div>
   </div>
+
   <div v-else style="background: #f5f6f7">
     <div class="pages">
       <span style="float: left">
         <div class="job-date">
-         {{ List[0].createdAt }}
+         {{ List[0].createdAt.substr(0, 10) }}
         </div>
-        <div v-for="item of List" >
-          <div class="job-article">
+        <div v-for="(item, index) of List" :key="item.id">
+          <div class="job-article" @click="changeClick(index)">
             <div class="job-title">
               {{ item.jobTitle }}
             </div>
@@ -54,6 +55,52 @@
               </span>
             </div>
           </div>
+
+          <div id="details" :class="{change: ind !== index}">
+            <div class="describe">
+              <br/>
+              职位行情
+            </div>
+            <hr/>
+            <div v-if="item.jobsArray!==undefined">
+              <div v-for="number of num">
+                <a :href="item.jobsArray[number].url" target="_blank">
+                  <div class="d-title">
+                    {{ item.jobsArray[number].title }} · {{ item.jobsArray[number].company }}
+                  </div>
+                </a>
+                <span class="salary" v-if="item.jobsArray[number].salaryLower==-1">
+                  面议
+                </span>
+                <span class="salary" v-else>
+                  {{ item.jobsArray[number].salaryLower }}k - {{ item.jobsArray[number].salaryUpper }}k
+                </span>
+
+                <span class="experience" v-if="item.jobsArray[number].experienceLower==-1&&item.jobsArray[number].experienceUpper==-1">
+                  经验不限
+                </span>
+                <span class="experience" v-if="item.jobsArray[number].experienceUpper==-1&&item.jobsArray[number].experienceLower!==-1">
+                  {{ item.jobsArray[number].experienceLower }}年以上
+                </span>
+                <span class="experience" v-if="item.jobsArray[number].experienceUpper>0&&item.jobsArray[number].experienceLower>0">
+                    {{ item.jobsArray[number].experienceLower }}年 - {{ item.jobsArray[number].experienceUpper }}年
+                </span>
+
+                <span class="city">
+                  {{ item.jobsArray[number].city }}
+                </span>
+                <span class="company">
+                  {{ item.jobsArray[number].siteName }}
+                </span>
+                <hr/>
+              </div>
+              <div class="wait">
+                <br/>
+                  查看全部{{ item.jobCount }}个职位,敬请期待
+                <pre><br/></pre>
+              </div>
+            </div>
+         </div>
         </div>
       </span>
       <brief></brief>
@@ -69,6 +116,8 @@
     props: ['List', 'msg'],
     data() {
       return {
+        num: [0, 1, 2, 3],
+        ind: ''
       }
     },
     components: {
@@ -79,7 +128,13 @@
       handleClick: function (msg) {
         this.$emit('change', msg)
       },
-    }
+      changeClick: function (index) {
+        if (this.ind === index)
+          this.ind = ''
+        else
+          this.ind = index
+      }
+    },
   }
 </script>
 
@@ -115,9 +170,9 @@
   .job-article
     width 38rem
     height 8.5rem
-    margin-bottom 1rem
+    margin-top 1rem
     background #ffffff
-    box-shadow 0 2px 6px -1px rgba(0,0,0,.06)
+    box-shadow 0 1px 6px -2px rgba(0, 0, 0, .6)
   .job-title
     display inline-block
     font-size 17px
@@ -139,4 +194,38 @@
     font-size 16px
     color rgba(0, 0, 0, .65)
     margin-left 1.3rem
+  #details
+    overflow hidden
+    background #ffffff
+    box-shadow 0 1px 6px -2px rgba(0, 0, 0, .6)
+  .describe
+    margin-left 1.3rem
+    color #607d9b
+    font-size 15px
+  .d-title
+    margin-top 1rem
+    margin-left 2rem
+  .salary
+    display inline-block
+    color #dc143c
+    margin-left 2rem
+    margin-top .5rem
+  .experience
+    margin-left .7rem
+    color #607d9b
+    font-size 15px
+  .city
+    margin-left 1rem
+    color #607d9b
+    font-size 15px
+  .company
+    margin-right 1rem
+    color #607d9b
+    font-size 15px
+    float: right
+  .wait
+    text-align center
+    color #607d9b
+  .change
+    height 0
 </style>
